8e1fc26450b22d356a3c1cce2b6b0a21412830a1
y123ob <y123ob@naver.com>
2020-08-15 18:27:33 +0900

    ver 1
    닉네임, 직업, 레벨, 접속일수, 무릉

---
+++ guildlist.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import re
+import pandas
+import requests
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+driver = webdriver.Chrome('./chromedriver')
+
+driver.get('https://maple.gg/guild/elysium/%ED%9D%91%EB%AC%98')
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+guild_content = soup.find_all(class_ = 'col-lg-3 col-md-6 col-sm-6 mt-4')
+
+csv_mode = True
+
+if(csv_mode):
+    csv_file = open("guild_list.csv", "w")
+    csv_file.write("닉네임,직업,레벨,최근 접속 일수,무릉층수,무릉시간\n")
+
+for member in guild_content:
+    
+    nickname = member.find(class_ = re.compile('font-size-14 +')).get_text()
+    job_level = member.find(class_ = 'font-size-12').get_text()
+    last_summary_date = member.find(class_ = 'user-summary-date').get_text()
+    job, level = job_level.split('/Lv.')
+    job = job.replace(',' , '')
+    last_summary_date = last_summary_date[9:-3]
+    if(last_summary_date == '알'):
+        last_summary_date = '-'
+    
+    nick_url = 'https://maple.gg/u/' + nickname
+    webpage = requests.get(nick_url)
+    nick_soup = BeautifulSoup(webpage.content, "html.parser")
+    floor = nick_soup.find(class_ = 'user-summary-floor font-weight-bold')
+    floor_time = nick_soup.find(class_ = 'user-summary-duration')
+    if(floor == None):
+        floor = '-'
+    else:
+        floor = int(floor.get_text()[:-1])
+    if(floor_time == None):
+        floor_time = '-'
+    else:
+        floor_time = floor_time.get_text()
+    
+    if(csv_mode):
+        csv_file.write(nickname+','+ job +','+ level +','+ last_summary_date +\
+            ','+str(floor)+','+floor_time+'\n')
+    
+    
